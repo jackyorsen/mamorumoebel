c8bd51f8e7c3f435401ad6695d6bee568d9fbbbb
jackyorsen <jackyorsen@gmail.com>
2025-12-08 20:39:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,22 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    base: '/mamorumoebel/',  // <<< WICHTIG!
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
+    base: '/mamorumoebel/',   // <<< Wichtig für GitHub Pages
+
     plugins: [react()],
+
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './'),
-      }
-    }
-  };
-});
+        '@': '/src',
+      },
+    },
+  }
+})
